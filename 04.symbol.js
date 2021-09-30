9614830f64795f0358569106f140db50f538ab0f
jayteen <delspy@qq.com>
2021-09-30 14:27:53 +0800

    symbol training

--- 04.symbol.js
+++ 04.symbol.js
@@ -6,13 +6,36 @@
     
     const c = Symbol('c')
     const d = Symbol('d')
+    const e = Symbol.for('e')
     const obj = {
         id: '111',
         name: 'xuqicheng',
-        c,
-        d
+        [c]: 'Symbol(\'c\')',
+        [d](){},
+        [e]: 'Symbol.for(\'e\')'
     }
     console.log(obj)
     
     console.log(Object.getOwnPropertySymbols(obj))
+    
+    console.log('-------------------------')
+
+    console.log(c)
+    console.log(Symbol.for('c'))
+    console.log(c === Symbol.for('c'))
+    console.log(obj[Symbol.for('c')])
+
+    console.log('-------------------------')
+
+    const m = Symbol.for('m')
+    const n = Symbol.for('m')
+    console.log(m === n)
+
+    console.log('-------------------------')
+
+    console.log(Symbol.for('e'))
+    console.log(e === Symbol.for('e'))
+    console.log(obj[Symbol.for('e')])
+
+
 })()
